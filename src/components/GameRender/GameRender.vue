a6e987dc114a545194a75870ddb277d845215bf5
Kyle De Sousa <kyledesousa@finestdental.co.uk>
2020-01-28 22:43:56 +0000

    added vue

---
+++ src/components/GameRender/GameRender.vue
@@ -0,0 +1,36 @@
+<template>
+    
+    <ul class="asd" >
+           <li v-for="(game, index) in games">
+            <p> {{ index }} <strong>{{ game.title }}</strong></p>
+           </li>
+    </ul>
+
+
+</template>
+
+<script>
+    import axios from 'axios';
+    
+    export default {
+        data() {
+            return {
+                games: []
+            }
+        },
+        
+        // Fetches games when the component is created.
+        created() {
+            axios.get(`json/games.json`)
+                    .then(response => {
+                        // JSON responses are automatically parsed.
+                        this.games = response.data.gamesData
+                      
+                     
+                    })
+                    .catch(e => {
+                        this.errors.push(e)
+                    })
+        }
+    }
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <GameRender/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import NavComp from '@/components/NavComp/NavComp.vue'
+import GameRender from '@/components/GameRender/GameRender.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+    NavComp,
+    GameRender
+  }
+}
+</script>
